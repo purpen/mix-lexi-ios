32b72f433b54c167820e969e437eecb1ad973774
PersonalRhp <1836619909@qq.com>
2018-07-25 18:07:02 +0800

    首页搜索框，tab

---
+++ lexi/AppEntry/Feature/Home/Controller/THNLivingHallViewController.m
@@ -0,0 +1,25 @@
+//
+//  THNLivingHallViewController.m
+//  lexi
+//
+//  Created by HongpingRao on 2018/7/25.
+//  Copyright © 2018年 taihuoniao. All rights reserved.
+//
+
+#import "THNLivingHallViewController.h"
+
+@interface THNLivingHallViewController ()
+
+@end
+
+@implementation THNLivingHallViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+
+
+@end
