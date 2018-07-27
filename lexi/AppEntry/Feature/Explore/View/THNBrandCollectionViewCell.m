0ed1989e4005d259b4004c617967c763961c3fe9
PersonalRhp <1836619909@qq.com>
2018-07-27 18:02:32 +0800

    探索界面UI的实现

---
+++ lexi/AppEntry/Feature/Explore/View/THNBrandCollectionViewCell.m
@@ -0,0 +1,18 @@
+//
+//  THNBrandCollectionViewCell.m
+//  lexi
+//
+//  Created by HongpingRao on 2018/7/27.
+//  Copyright © 2018年 taihuoniao. All rights reserved.
+//
+
+#import "THNBrandCollectionViewCell.h"
+
+@implementation THNBrandCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
---
+++ lexi/AppEntry/Feature/Explore/View/THNCategoriesCollectionView.h
@@ -0,0 +1,17 @@
+//
+//  THNCategoriesCollectionView.h
+//  lexi
+//
+//  Created by HongpingRao on 2018/7/26.
+//  Copyright © 2018年 taihuoniao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THNCategoriesCollectionView : UICollectionView
+
+- (instancetype)initWithFrame:(CGRect)frame
+         collectionViewLayout:(UICollectionViewLayout *)layout
+                withDataArray:(NSArray *)dataArray;
+
+@end
