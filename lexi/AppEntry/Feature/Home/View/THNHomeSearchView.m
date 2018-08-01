f99f9ad6a62bd7ccdbbba1d491867902623a8e7e
PersonalRhp <1836619909@qq.com>
2018-08-01 18:13:47 +0800

    生活馆UI的基本实现

--- lexi/AppEntry/Feature/Home/View/THNHomeSearchView.m
+++ lexi/AppEntry/Feature/Home/View/THNHomeSearchView.m
@@ -25,15 +25,9 @@
     self = [super init];
     if (self) {
         self = [THNHomeSearchView viewFromXib];
-        self.layer.shadowRadius = 5;
-        self.layer.cornerRadius = 5;
-        self.layer.shadowOffset = CGSizeMake(5, 5);
-        self.layer.borderWidth = 0.5;
-        self.layer.shadowOpacity = 0.1;
-        self.layer.shadowColor = [[UIColor colorWithHexString:@"000000"] CGColor];
-        self.layer.borderColor = [[UIColor colorWithHexString:@"e9e9e9"] CGColor];
+        
         [self.searchTextField setValue:[UIColor colorWithHexString:@"555555"] forKeyPath:@"_placeholderLabel.textColor"];
-        self.layer.masksToBounds = NO;
+        [self drwaShadow];
     }
     return self;
 }
---
+++ lexi/AppEntry/Feature/LivingHall/View/THNLivingHallHeaderView.h
@@ -0,0 +1,15 @@
+//
+//  THNLivingHallHeaderView.h
+//  lexi
+//
+//  Created by HongpingRao on 2018/8/1.
+//  Copyright © 2018年 taihuoniao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THNLivingHallHeaderView : UIView
+
+@property (weak, nonatomic) IBOutlet UIView *noProductView;
+
+@end
