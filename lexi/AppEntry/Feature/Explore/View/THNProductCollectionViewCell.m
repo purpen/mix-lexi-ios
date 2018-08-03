a408723622242f4282a3ed07e2ceb9bf9da15066
PersonalRhp <1836619909@qq.com>
2018-08-03 17:52:38 +0800

    生活馆界面UI各个机型的适配修改

--- lexi/AppEntry/Feature/Explore/View/THNProductCollectionViewCell.m
+++ lexi/AppEntry/Feature/Explore/View/THNProductCollectionViewCell.m
@@ -16,6 +16,8 @@
 @property (weak, nonatomic) IBOutlet UIImageView *productImageView;
 @property (weak, nonatomic) IBOutlet UILabel *productNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *productPriceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *producrOriginalPriceLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *shippingImageView;
 
 @end
 
@@ -28,10 +30,12 @@
     
 }
 
-- (void)setProductMode:(THNProductModel *)productModel {
-    [self.productImageView sd_setImageWithURL:[NSURL URLWithString:productModel.cover]];
+- (void)setProductModel:(THNProductModel *)productModel {
+    _productModel = productModel;
+    [self.productImageView sd_setImageWithURL:[NSURL URLWithString:@"https://kg.erp.taihuoniao.com/20180711/1808FgkTUxcFE3_2DAXlTdi4rQMRU7IY.jpg"]];
     self.productNameLabel.text = productModel.name;
     self.productPriceLabel.text = [NSString stringWithFormat:@"%2.f",productModel.min_sale_price];
+    self.producrOriginalPriceLabel.text = [NSString stringWithFormat:@"喜欢 +%ld",productModel.like_count];
 }
 
 @end
