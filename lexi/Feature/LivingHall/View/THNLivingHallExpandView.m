43489a579184fe4d770a9a738ee6719d99e0c152
PersonalRhp <1836619909@qq.com>
2018-08-09 18:08:06 +0800

    生活馆UI改动以及调换接口数据

---
+++ lexi/Feature/LivingHall/View/THNLivingHallExpandView.m
@@ -0,0 +1,19 @@
+//
+//  THNLivingHallExpandView.m
+//  lexi
+//
+//  Created by HongpingRao on 2018/8/9.
+//  Copyright © 2018年 taihuoniao. All rights reserved.
+//
+
+#import "THNLivingHallExpandView.h"
+
+@implementation THNLivingHallExpandView
+
+- (IBAction)open:(id)sender {
+    
+    self.loadMoreDateBlcok();
+}
+
+
+@end
--- lexi/Feature/LivingHall/View/THNLivingHallRecommendTableViewCell.m
+++ lexi/Feature/LivingHall/View/THNLivingHallRecommendTableViewCell.m
@@ -12,13 +12,28 @@
 #import "THNSetModel.h"
 #import "UIView+Helper.h"
 #import "THNMarco.h"
+#import "THNUserPartieModel.h"
+#import "THNProductModel.h"
+#import "UIImageView+WebCache.h"
+#import <MJExtension/MJExtension.h>
+#import "THNAPI.h"
+#import "THNObtainedView.h"
 
 static NSString *const krecommendCellIdentifier = @"krecommendCellIdentifier";
+// 喜欢商品的用户
+static NSString *const kUrlLikeProductUser = @"/product/userlike";
 
 @interface THNLivingHallRecommendTableViewCell()<UICollectionViewDataSource>
 
 @property (weak, nonatomic) IBOutlet UICollectionView *recommendCollectionView;
-
+@property (weak, nonatomic) IBOutlet UIImageView *productImageView;
+@property (weak, nonatomic) IBOutlet UILabel *productNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *productPriceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *producrOriginalPriceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *likeCountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *recommenDationLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *curatorAvatarImageView;
+@property (nonatomic, strong) NSArray *likeProductUserArray;
 
 @end
 
@@ -26,35 +41,53 @@ static NSString *const krecommendCellIdentifier = @"krecommendCellIdentifier";
 
 - (void)awakeFromNib {
     [super awakeFromNib];
+    [self.productImageView drawCornerWithType:0 radius:4];
+    [self.curatorAvatarImageView drawCornerWithType:0 radius:4];
     self.recommendCollectionView.dataSource = self;
     UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc]initWithLineSpacing:5 initWithWidth:25 initwithHeight:25];
     [self.recommendCollectionView setCollectionViewLayout:flowLayout];
     [self.recommendCollectionView registerNib:[UINib nibWithNibName:@"THNBannnerCollectionViewCell" bundle:nil] forCellWithReuseIdentifier:krecommendCellIdentifier];
 }
 
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
+- (void)loadLikeProductUserData:(NSString *)rid {
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"rid"] = rid;
+    THNRequest *request = [THNAPI getWithUrlString:kUrlLikeProductUser requestDictionary:params isSign:YES delegate:nil];
+    [request startRequestSuccess:^(THNRequest *request, THNResponse *result) {
+        self.likeProductUserArray = result.data[@"product_like_users"];
+        [self.recommendCollectionView reloadData];
+    } failure:^(THNRequest *request, NSError *error) {
+        
+    }];
 }
 
-//// 设置 cell 与 cell 之间的间距
-//- (void)setFrame:(CGRect)frame{
-//    frame.origin.y += 15;
-//    frame.size.height -= 15;
-//    [super setFrame:frame];
-//}
+- (IBAction)delete:(id)sender {
+    [THNObtainedView show];
+}
+
+- (void)setProductModel:(THNProductModel *)productModel {
+    _productModel = productModel;
+    [self.productImageView sd_setImageWithURL:[NSURL URLWithString:productModel.cover]];
+    self.productNameLabel.text = productModel.name;
+    self.productPriceLabel.text = [NSString stringWithFormat:@"%2.f",productModel.min_sale_price];
+    self.producrOriginalPriceLabel.text = [NSString stringWithFormat:@"%2.f",productModel.min_price];
+    self.likeCountLabel.text = [NSString stringWithFormat:@"喜欢 +%ld",productModel.like_count];
+    self.recommenDationLabel.text = productModel.features;
+}
+
+// 馆长信息头像
+- (void)setCurtorAvatar:(NSString *)storeAvatarUrl {
+    [self.curatorAvatarImageView sd_setImageWithURL:[NSURL URLWithString:storeAvatarUrl]];
+}
 
 - (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return  3;
+   return self.likeProductUserArray.count < 3 ? self.likeProductUserArray.count : 3;
 }
 
 - (UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
     THNBannnerCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:krecommendCellIdentifier forIndexPath:indexPath];
-    THNSetModel *model = [[THNSetModel alloc]init];
-    model.type = @"avatar";
-    [cell setSetModel:model];
+    THNUserPartieModel *userPatieModel = [THNUserPartieModel mj_objectWithKeyValues:self.likeProductUserArray[indexPath.row]];
+    [cell setUserPartieModel:userPatieModel];
     return cell;
 }
 
