0ecd4fdadae3efca11e7480de166c4ee5ac5ac48
FFynn <y809204367@163.com>
2018-08-23 22:47:09 +0800

    调整Network工具类，默认添加签名；替换请求方法

--- lexi/Feature/LivingHall/View/THNLivingHallRecommendTableViewCell.m
+++ lexi/Feature/LivingHall/View/THNLivingHallRecommendTableViewCell.m
@@ -60,7 +60,7 @@ static NSString *const kUrlProductUserLike = @"/product/userlike";
 - (void)loadLikeProductUserData:(NSString *)rid {
     NSMutableDictionary *params = [NSMutableDictionary dictionary];
     params[@"rid"] = rid;
-    THNRequest *request = [THNAPI getWithUrlString:kUrlProductUserLike requestDictionary:params isSign:YES delegate:nil];
+    THNRequest *request = [THNAPI getWithUrlString:kUrlProductUserLike requestDictionary:params delegate:nil];
     [request startRequestSuccess:^(THNRequest *request, THNResponse *result) {
         self.likeProductUserArray = result.data[@"product_like_users"];
         [self.recommendCollectionView reloadData];
@@ -73,7 +73,7 @@ static NSString *const kUrlProductUserLike = @"/product/userlike";
 - (void)addLikeUser:(NSString *)rid {
     NSMutableDictionary *params = [NSMutableDictionary dictionary];
     params[@"rid"] = rid;
-    THNRequest *request = [THNAPI postWithUrlString:kUrlUserLike requestDictionary:params isSign:YES delegate:nil];
+    THNRequest *request = [THNAPI postWithUrlString:kUrlUserLike requestDictionary:params delegate:nil];
     [request startRequestSuccess:^(THNRequest *request, THNResponse *result) {
         [self loadLikeProductUserData:rid];
     } failure:^(THNRequest *request, NSError *error) {
@@ -85,7 +85,7 @@ static NSString *const kUrlProductUserLike = @"/product/userlike";
 - (void)removeLikeUser:(NSString *)rid {
     NSMutableDictionary *params = [NSMutableDictionary dictionary];
     params[@"rid"] = rid;
-    THNRequest *request = [THNAPI deleteWithUrlString:kUrlUserLike requestDictionary:params isSign:YES delegate:nil ];
+    THNRequest *request = [THNAPI deleteWithUrlString:kUrlUserLike requestDictionary:params delegate:nil ];
     [request startRequestSuccess:^(THNRequest *request, THNResponse *result) {
         [self loadLikeProductUserData:rid];
     } failure:^(THNRequest *request, NSError *error) {
