9e6f5e4947a9feeaf0a53c2607790956cdff00db
PersonalRhp <1836619909@qq.com>
2018-08-30 18:08:46 +0800

    首页样式调整以及优惠劵数据嵌套，所有公告实现

--- lexi/Feature/BrandHall/View/THNCouponView.m
+++ lexi/Feature/BrandHall/View/THNCouponView.m
@@ -8,12 +8,14 @@
 
 #import "THNCouponView.h"
 #import "UIColor+Extension.h"
+#import "THNLoginManager.h"
 
 @interface THNCouponView()
 
 @property (weak, nonatomic) IBOutlet UIButton *receiveButton;
 @property (weak, nonatomic) IBOutlet UIView *redEnvelopeView;
 @property (weak, nonatomic) IBOutlet UIView *fullReductionView;
+@property (weak, nonatomic) IBOutlet UILabel *fullReductionLabel;
 
 @end
 
@@ -25,6 +27,43 @@
     self.receiveButton.layer.borderWidth = 1;
     self.receiveButton.layer.borderColor = [UIColor colorWithHexString:@"DADADA"].CGColor;
     self.receiveButton.layer.cornerRadius = 13;
+}
+
+- (void)layoutCouponView:(NSArray *)fullReductions 
+        withLoginCoupons:(NSArray *)loginCoupons
+       withNologinCoupos:(NSArray *)noLoginCoupons
+         withHeightBlock:(CouponViewHeightBlock)couponViewHeightBlock {
+    
+    CGFloat couponViewHeight = 0.0;
+    
+    if ([THNLoginManager isLogin]) {
+        self.redEnvelopeView.hidden = noLoginCoupons.count == 0;
+    } else {
+        self.redEnvelopeView.hidden = loginCoupons.count == 0;
+    }
+    
+    self.fullReductionView.hidden = fullReductions.count == 0;
+    
+    if (self.redEnvelopeView.hidden) {
+        couponViewHeight = 39;
+    } else if (self.fullReductionView.hidden) {
+        couponViewHeight = 50;
+    } else if (self.redEnvelopeView.hidden && self.fullReductionView.hidden) {
+        couponViewHeight = 0;
+    } else {
+        couponViewHeight = 65;
+    }
+    
+    NSMutableString *mutableString = [[NSMutableString alloc]init];
+   
+    for (NSDictionary *dict in fullReductions) {
+         NSString *fullReductionStr = [NSString stringWithFormat:@"  %@",dict[@"type_text"]];
+        [mutableString appendString:fullReductionStr];
+    }
+    
+    self.fullReductionLabel.text = mutableString;
+    
+    couponViewHeightBlock(couponViewHeight);
 }
 
 - (IBAction)receive:(id)sender {
