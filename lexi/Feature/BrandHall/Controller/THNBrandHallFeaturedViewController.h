21b2725faef85b851e43a4d61aaf2ef113ddd6d8
PersonalRhp <1836619909@qq.com>
2018-09-03 19:04:30 +0800

    领取优惠券实现,以及品牌馆精选实现

---
+++ lexi/Feature/BrandHall/Controller/THNBrandHallFeaturedViewController.h
@@ -0,0 +1,13 @@
+//
+//  THNBrandHallFeaturedViewController.h
+//  lexi
+//
+//  Created by HongpingRao on 2018/9/3.
+//  Copyright © 2018年 taihuoniao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THNBrandHallFeaturedViewController : UIViewController
+
+@end
