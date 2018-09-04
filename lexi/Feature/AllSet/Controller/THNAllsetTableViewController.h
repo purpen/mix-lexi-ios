88d13a4bc485ec107bbf828983c40be4fbc9cfd0
PersonalRhp <1836619909@qq.com>
2018-09-04 12:59:39 +0800

    集合的实现

---
+++ lexi/Feature/AllSet/Controller/THNAllsetTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  THNAllsetTableViewController.h
+//  lexi
+//
+//  Created by rhp on 2018/9/2.
+//  Copyright © 2018年 taihuoniao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "THNBaseTableViewController.h"
+
+@interface THNAllsetTableViewController : UITableViewController
+
+@end
---
+++ lexi/Feature/AllSet/View/THNAllsetTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  THNAllsetTableViewCell.h
+//  lexi
+//
+//  Created by rhp on 2018/9/2.
+//  Copyright © 2018年 taihuoniao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class THNCollectionModel;
+
+@interface THNAllsetTableViewCell : UITableViewCell
+
+@property (nonatomic, strong) THNCollectionModel *collectionModel;
+
+@end
