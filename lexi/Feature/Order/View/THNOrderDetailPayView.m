705bda56adee2c958759c80fc7f671864818775c
PersonalRhp <1836619909@qq.com>
2018-09-11 22:09:55 +0800

    收货地址UI样式的实现

--- lexi/Feature/Order/View/THNOrderDetailPayView.m
+++ lexi/Feature/Order/View/THNOrderDetailPayView.m
@@ -7,6 +7,7 @@
 //
 
 #import "THNOrderDetailPayView.h"
+#import "THNOrderDetailModel.h"
 
 @interface THNOrderDetailPayView()
 @property (weak, nonatomic) IBOutlet UILabel *orderNumberLabel;
@@ -19,5 +20,20 @@
 @end
 
 @implementation THNOrderDetailPayView
+
+- (void)setDetailModel:(THNOrderDetailModel *)detailModel {
+    _detailModel = detailModel;
+    self.orderNumberLabel.text = detailModel.outside_target_id;
+    self.payMethodLabel.text = @"微信在线支付";
+    self.payMethodImageView.image = [UIImage imageNamed:@"icon_order_wechat"];
+    self.subtotalLabel.text = [NSString stringWithFormat:@"¥%.2f",detailModel.total_amount];
+    self.totalMoneyLabel.text = [NSString stringWithFormat:@"¥%.2f",detailModel.pay_amount];
+    
+    if (detailModel.freight == 0) {
+        self.deliveryMethodLabel.text  = @"包邮";
+    } else {
+        self.deliveryMethodLabel.text = [NSString stringWithFormat:@"¥%.2f",detailModel.freight];
+    }
+}
 
 @end
