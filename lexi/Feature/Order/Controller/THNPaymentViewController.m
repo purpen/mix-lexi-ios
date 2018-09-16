26a84a6ec4210a15cdf09b9d845b73550260e7e0
Fynn <flyang163@gmail.com>
2018-09-16 12:22:39 +0800

    添加支付方式

--- lexi/Feature/Order/Controller/THNPaymentViewController.m
+++ lexi/Feature/Order/Controller/THNPaymentViewController.m
@@ -8,15 +8,25 @@
 
 #import "THNPaymentViewController.h"
 #import "THNBuyProgressView.h"
+#import "THNPaymentTableViewCell.h"
+#import "THNHeaderTitleView.h"
+#import "THNPaymentPriceView.h"
 
-static NSString *kTitleDone = @"确认下单";
+static NSString *kTitleDone     = @"确认下单";
+static NSString *kTextPayment   = @"选择支付方式";
 
-@interface THNPaymentViewController ()
+@interface THNPaymentViewController () <UITableViewDelegate, UITableViewDataSource>
 
 /// 进度条
 @property (nonatomic, strong) THNBuyProgressView *progressView;
 /// 完成按钮
 @property (nonatomic, strong) UIButton *doneButton;
+/// 支付方式
+@property (nonatomic, strong) UITableView *paymentTable;
+/// 价格
+@property (nonatomic, strong) THNPaymentPriceView *priceView;
+///
+@property (nonatomic, strong) NSIndexPath *selectIndex;
 
 @end
 
@@ -30,7 +40,27 @@ static NSString *kTitleDone = @"确认下单";
 
 #pragma mark - event response
 - (void)doneButtonAction:(UIButton *)button {
-    [SVProgressHUD showSuccessWithStatus:@"支付"];
+    switch ([self thn_getPaymentType]) {
+        case THNPaymentTypeWechat: {
+            [SVProgressHUD showSuccessWithStatus:@"微信支付"];
+        }
+            break;
+            
+        case THNPaymentTypeAlipay: {
+            [SVProgressHUD showSuccessWithStatus:@"支付宝"];
+        }
+            break;
+            
+        case THNPaymentTypeHuabei: {
+            [SVProgressHUD showSuccessWithStatus:@"花呗"];
+        }
+            break;
+    }
+}
+
+#pragma mark - private methods
+- (THNPaymentType)thn_getPaymentType {
+    return (THNPaymentType)self.selectIndex.row;
 }
 
 #pragma mark - setup UI
@@ -38,7 +68,12 @@ static NSString *kTitleDone = @"确认下单";
     self.view.backgroundColor = [UIColor colorWithHexString:@"#F7F9FB"];
     
     [self.view addSubview:self.progressView];
+    [self.view addSubview:self.paymentTable];
     [self.view addSubview:self.doneButton];
+    
+    // 示例
+    [self.priceView thn_setPriceValue:1852.2 totalPriceValue:1867.2 freightValue:15];
+    self.paymentTable.tableHeaderView = self.priceView;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -51,6 +86,55 @@ static NSString *kTitleDone = @"确认下单";
     self.navigationBarView.title = kTitlePayment;
 }
 
+#pragma mark - tableView datasource & delegate
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 58.0;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 44.0;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    THNHeaderTitleView *headerView = [[THNHeaderTitleView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 44)
+                                                                         title:kTextPayment];
+    
+    return headerView;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    return 0.01;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
+    return [UIView new];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    THNPaymentTableViewCell *paymentCell = [THNPaymentTableViewCell initPaymentCellWithTableView:tableView];
+    [paymentCell thn_setPaymentTypeWithType:(THNPaymentType)indexPath.row];
+    
+    return paymentCell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    THNPaymentTableViewCell *selectedCell = [tableView cellForRowAtIndexPath:self.selectIndex];
+    selectedCell.selected = NO;
+    
+    THNPaymentTableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    cell.selected = YES;
+
+    self.selectIndex = indexPath;
+}
+
 #pragma mark - getters and setters
 - (THNBuyProgressView *)progressView {
     if (!_progressView) {
@@ -58,6 +142,31 @@ static NSString *kTitleDone = @"确认下单";
                                                             index:2];
     }
     return _progressView;
+}
+
+- (UITableView *)paymentTable {
+    if (!_paymentTable) {
+        CGFloat originBottom = kDeviceiPhoneX ? 82 : 50;
+        _paymentTable = [[UITableView alloc] initWithFrame: \
+                         CGRectMake(0, CGRectGetMaxY(self.progressView.frame), SCREEN_WIDTH, SCREEN_HEIGHT - CGRectGetMaxY(self.progressView.frame) - originBottom)
+                                                     style:(UITableViewStyleGrouped)];
+        _paymentTable.delegate = self;
+        _paymentTable.dataSource = self;
+        _paymentTable.separatorStyle = UITableViewCellSeparatorStyleNone;
+        _paymentTable.tableFooterView = [UIView new];
+        _paymentTable.showsVerticalScrollIndicator = NO;
+        _paymentTable.contentInset = UIEdgeInsetsMake(0, 0, 0, 0);
+        _paymentTable.bounces = NO;
+        _paymentTable.backgroundColor = [UIColor colorWithHexString:@"#F7F9FB"];
+    }
+    return _paymentTable;
+}
+
+- (THNPaymentPriceView *)priceView {
+    if (!_priceView) {
+        _priceView = [[THNPaymentPriceView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 95)];
+    }
+    return _priceView;
 }
 
 - (UIButton *)doneButton {
