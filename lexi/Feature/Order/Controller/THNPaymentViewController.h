925164f0a2e50bb222dabb49acf8f047fa2fafab
Fynn <y809204367@163.com>
2018-09-16 10:43:35 +0800

    添加付款方式

---
+++ lexi/Feature/Order/Controller/THNPaymentViewController.h
@@ -0,0 +1,13 @@
+//
+//  THNPaymentViewController.h
+//  lexi
+//
+//  Created by FLYang on 2018/9/16.
+//  Copyright © 2018年 taihuoniao. All rights reserved.
+//
+
+#import "THNBaseViewController.h"
+
+@interface THNPaymentViewController : THNBaseViewController
+
+@end
