9110190e565de8ab6acd46c3d9ced49607de4d42
rhp <rhp@rhpdeMacBook-Pro.local>
2018-09-16 10:33:10 +0800

    。。。

---
+++ lexi/Feature/Order/Model/THNFullReductionModel.m
@@ -0,0 +1,13 @@
+//
+//  THNFullReductionModel.m
+//  lexi
+//
+//  Created by rhp on 2018/9/16.
+//  Copyright © 2018年 taihuoniao. All rights reserved.
+//
+
+#import "THNFullReductionModel.h"
+
+@implementation THNFullReductionModel
+
+@end
