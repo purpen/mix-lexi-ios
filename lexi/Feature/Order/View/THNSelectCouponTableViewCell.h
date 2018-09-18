16add1710ff43b9d3a00ffc727ec688e1db68520
PersonalRhp <1836619909@qq.com>
2018-09-18 18:02:18 +0800

    品牌馆的样式调整，领取优惠券实现C

---
+++ lexi/Feature/Order/View/THNSelectCouponTableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  THNSelectCouponTableViewCell.h
+//  lexi
+//
+//  Created by HongpingRao on 2018/9/17.
+//  Copyright © 2018年 taihuoniao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class THNCouponModel;
+
+@interface THNSelectCouponTableViewCell : UITableViewCell
+
+@property (nonatomic, strong) THNCouponModel *couponModel;
+
+
+@end
