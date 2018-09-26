1bb56990c5a04b0710df1993178d62677871d7b7
PersonalRhp <1836619909@qq.com>
2018-09-26 18:00:23 +0800

    搜索UI的实现

---
+++ lexi/Feature/Search/View/THNSearchHotSearchCollectionViewCell.m
@@ -0,0 +1,18 @@
+//
+//  THNSearchHotSearchCollectionViewCell.m
+//  lexi
+//
+//  Created by HongpingRao on 2018/9/26.
+//  Copyright © 2018年 taihuoniao. All rights reserved.
+//
+
+#import "THNSearchHotSearchCollectionViewCell.h"
+
+@implementation THNSearchHotSearchCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
