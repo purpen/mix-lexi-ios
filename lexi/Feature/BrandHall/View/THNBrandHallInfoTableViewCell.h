a1b246c13ab2dd23827ca11fc476ce46fbb9e1e0
PersonalRhp <1836619909@qq.com>
2018-10-18 22:26:07 +0800

    关于品牌馆UI的实现

---
+++ lexi/Feature/BrandHall/View/THNBrandHallInfoTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  THNBrandHallInfoTableViewCell.h
+//  lexi
+//
+//  Created by rhp on 2018/10/18.
+//  Copyright © 2018年 taihuoniao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THNBrandHallInfoTableViewCell : UITableViewCell
+
+@end
