c56e7fa88e4dcaa417f767a59e8c62cb4336691c
FFynn <y809204367@163.com>
2018-10-26 17:08:59 +0800

    new

--- lexi/Feature/Login/Controller/THNSignInViewController.m
+++ lexi/Feature/Login/Controller/THNSignInViewController.m
@@ -116,6 +116,8 @@ static NSString *const kTextSkip            = @"跳过";
 
 #pragma mark - custom delegate
 - (void)thn_signInWithParam:(NSDictionary *)param loginModeType:(THNLoginModeType)type {
+    [SVProgressHUD thn_show];
+    
     WEAKSELF;
     
     [THNLoginManager userLoginWithParams:param modeType:type completion:^(THNResponse *result, NSError *error) {
@@ -130,6 +132,7 @@ static NSString *const kTextSkip            = @"跳过";
         }
         
         [weakSelf thn_loginSuccessWithModeType:type];
+        [SVProgressHUD dismiss];
     }];
 }
 
