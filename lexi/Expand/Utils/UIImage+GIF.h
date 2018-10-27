828b747aaa65a9717614ce7776f530c368dbae21
FFynn <y809204367@163.com>
2018-10-27 17:50:39 +0800

    添加刷新&加载更多的分类；添加gif图拆解的工具类；用户列表&优惠券列表添加刷新&加载更多

--- lexi/Expand/Utils/UIImage+GIF.h
+++ lexi/Expand/Utils/UIImage+GIF.h
@@ -15,4 +15,12 @@ typedef void (^GifImageBlock)(UIImage *gifImage);
 + (UIImage *)imageWithGifNamed:(NSString *)name;
 + (UIImage *)imageWithGifData:(NSData *)data;
 
+/**
+ gif 图拆分成图片数组
+
+ @param name gif 图名称
+ @return 图片数组
+ */
++ (NSArray *)imagesWithGifNamed:(NSString *)name;
+
 @end
