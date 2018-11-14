c153906df789fcc391630f0c758353bc5995491b
PersonalRhp <1836619909@qq.com>
2018-11-14 21:06:36 +0800

    发布橱窗首页增加跳转，以及集合详情页增加遮罩，及图片变形修复

---
+++ lexi/Feature/ReleaseWindow/ViewController/THNSelectWindowProductViewController.h
@@ -0,0 +1,13 @@
+//
+//  THNSelectWindowProductViewController.h
+//  lexi
+//
+//  Created by HongpingRao on 2018/11/14.
+//  Copyright © 2018年 taihuoniao. All rights reserved.
+//
+
+#import "THNBaseViewController.h"
+
+@interface THNSelectWindowProductViewController : THNBaseViewController
+
+@end
---
+++ lexi/Feature/ReleaseWindow/ViewController/THNSelectWindowProductViewController.m
@@ -0,0 +1,37 @@
+//
+//  THNSelectWindowProductViewController.m
+//  lexi
+//
+//  Created by HongpingRao on 2018/11/14.
+//  Copyright © 2018年 taihuoniao. All rights reserved.
+//
+
+#import "THNSelectWindowProductViewController.h"
+
+@interface THNSelectWindowProductViewController ()
+
+@end
+
+@implementation THNSelectWindowProductViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
