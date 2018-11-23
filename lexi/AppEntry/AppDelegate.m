e64ac70919dcbcbde7b2deb7b39989837f5c0e0b
Fynn <flyang163@gmail.com>
2018-11-24 00:20:40 +0800

    修改URL Sch

--- lexi/AppEntry/AppDelegate.m
+++ lexi/AppEntry/AppDelegate.m
@@ -134,10 +134,10 @@ static NSString *const kCancelPayOrderTitle = @"取消支付";
     }
     
     /* 获取集成测试的 deviceID */
-    NSString *deviceID =  [UMConfigure deviceIDForIntegration];
+//    NSString *deviceID =  [UMConfigure deviceIDForIntegration];
     
 #ifdef DEBUG
-    NSLog(@"\n\n--- 集成测试的 deviceID:\n    %@\n", deviceID);
+//    NSLog(@"\n\n--- 集成测试的 deviceID:\n    %@\n", deviceID);
 #endif
 }
 
