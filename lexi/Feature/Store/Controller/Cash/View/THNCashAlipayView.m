115399362bfa0fcc6e038cd0205c261b2fac71dc
Fynn <flyang163@gmail.com>
2018-12-11 18:00:26 +0800

    用户活动提现视图

---
+++ lexi/Feature/Store/Controller/Cash/View/THNCashAlipayView.m
@@ -0,0 +1,21 @@
+//
+//  THNCashAlipayView.m
+//  lexi
+//
+//  Created by FLYang on 2018/12/11.
+//  Copyright © 2018 taihuoniao. All rights reserved.
+//
+
+#import "THNCashAlipayView.h"
+
+@implementation THNCashAlipayView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
